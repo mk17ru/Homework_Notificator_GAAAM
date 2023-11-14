f8416253ac369bb535d6c80d17417bdabffe25b6
Georgii <georgii.kashin@gmail.com>
2023-11-14 07:39:08 +0000

    Implemented add_deadline logic (works with ids only).

--- src/handlers/add_deadline_handler.py
+++ src/handlers/add_deadline_handler.py
@@ -13,9 +13,15 @@ from telegram.ext import (
 )
 
 
-ADD_SUBJECT, ADD_DEADLINE_NAME  = range(2)
+ADD_SUBJECT, ADD_ACTIVITY, ADD_DEADLINE  = range(3)
+admin_usernames = ["gkashin"]
 
 async def start_add_deadline_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    is_admin = update.message.from_user.username in admin_usernames
+    if not is_admin:
+        await update.message.reply_text("Sorry, you're not allowed to create a deadline.")
+        return ConversationHandler.END
+
     await update.message.reply_text("Введите предмет:")
 
     return ADD_SUBJECT
@@ -23,15 +29,31 @@ async def start_add_deadline_callback(update: Update, context: ContextTypes.DEFA
 async def add_subject_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     context.user_data["SUBJECT"] = update.message.text
 
-    await update.message.reply_text("Ввведите название дедлайна:")
+    await update.message.reply_text("Ввведите название активности:")
 
-    return ADD_DEADLINE_NAME
+    return ADD_ACTIVITY
 
+
+async def add_activity_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data["ACTIVITY"] = update.message.text
+
+    await update.message.reply_text("Ввведите дедлайн:")
+
+    return ADD_DEADLINE
 
 async def add_deadline_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     context.user_data["DEADLINE"] = update.message.text
-    print(context.user_data["SUBJECT"]) # сохранилось!
-    print(context.user_data["DEADLINE"]) # сохранилось!
+
+    activity_id = context.user_data["ACTIVITY"]
+    deadline = context.user_data["DEADLINE"]
+    
+    sql = "INSERT INTO deadlines(activity_id, deadline) values (%s, %s)"
+
+    cur = conn.cursor()
+    data = (activity_id, deadline)
+    cur.execute(sql, data)
+    conn.commit()
+    conn.close()
 
     return ConversationHandler.END
 
@@ -45,7 +67,10 @@ def add_deadline_builder() -> ConversationHandler:
             ADD_SUBJECT: [
                 MessageHandler(filters.TEXT & ~filters.COMMAND, add_subject_callback)
             ],
-            ADD_DEADLINE_NAME: [
+            ADD_ACTIVITY: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, add_activity_callback)
+            ],
+            ADD_DEADLINE: [
                 MessageHandler(filters.TEXT & ~filters.COMMAND, add_deadline_callback)
             ],
         },
