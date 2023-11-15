d4630e2555992db5e48a68b14be8a569583c7f36
Georgii <georgii.kashin@gmail.com>
2023-11-15 08:51:23 +0000

    Implemented logic of adding subjects/activities.

--- src/handlers/add_deadline_handler.py
+++ src/handlers/add_deadline_handler.py
@@ -15,9 +15,9 @@ from telegram.ext import (
 
 from src.handlers.handlers import cancel_callback
 from src.db.connection import conn
-from src.db.helpers import get_full_relation
+from src.db.helpers import get_full_relation, run_sql
 
-ADD_SUBJECT, ADD_ACTIVITY, ADD_DEADLINE  = range(3)
+CHOOSE_SUBJECT, ADD_SUBJECT, CHOOSE_ACTIVITY, ADD_ACTIVITY, ADD_DEADLINE, START = range(6)
 admin_usernames = ["gkashin", "imashevchenko", "chichaaaps"]
 
 async def start_add_deadline_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -26,50 +26,105 @@ async def start_add_deadline_callback(update: Update, context: ContextTypes.DEFA
         await update.message.reply_text("Sorry, you're not allowed to create a deadline.")
         return ConversationHandler.END
 
-    result = get_full_relation("subjects")
-    subjects = [KeyboardButton(row[1]) for row in result]
-    reply_markup = ReplyKeyboardMarkup([subjects], one_time_keyboard=True)
-
+    subjects = get_full_relation("SUBJECTS")
+    context.user_data["SUBJECTS"] = subjects
+    buttons = [KeyboardButton(subject[1]) for subject in subjects]
+    reply_markup = ReplyKeyboardMarkup([buttons, ['Создать новый']], one_time_keyboard=True)
     await update.message.reply_text("Выберите предмет", reply_markup=reply_markup)
 
-    return ADD_SUBJECT
+    return CHOOSE_SUBJECT
 
 async def add_subject_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     context.user_data["SUBJECT"] = update.message.text
     subject = context.user_data["SUBJECT"]
 
-    result = get_full_relation("activities")
-    activities = [row[2] for row in result]
-    reply_markup = ReplyKeyboardMarkup([activities], one_time_keyboard=True)
-    await update.message.reply_text("Выберите активность", reply_markup=reply_markup)
+    sql = 'INSERT INTO SUBJECTS(name) VALUES(%s) RETURNING id;'
+
+    result = run_sql(sql, (subject,))
+    if not result:
+        # Make it now await function
+        await update.message.reply_text(f"Предмет с именем {subject} уже существует.")
+        return START
+
+    subject_id = result[0][0]
+    context.user_data["SUBJECT_ID"] = subject_id
+
+    await update.message.reply_text("Введите название активности:")
 
     return ADD_ACTIVITY
 
+async def choose_subject_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data["SUBJECT"] = update.message.text
+    subject = context.user_data["SUBJECT"]
+    if subject == 'Создать новый':
+        await update.message.reply_text("Введите название предмета:")
+        return ADD_SUBJECT
+
+    subjects = context.user_data["SUBJECTS"]
+    subject_id = next(filter(lambda record: record[1] == subject, subjects), None)[0]
+    context.user_data["SUBJECT_ID"] = subject_id
+
+    sql = 'SELECT * FROM ACTIVITIES WHERE SUBJECT_ID=%s;'
+    activities = run_sql(sql, (subject_id,))
+    context.user_data["ACTIVITIES"] = activities
+
+    buttons = [activity[2] for activity in activities]
+    reply_markup = ReplyKeyboardMarkup([buttons, ['Создать новую']], one_time_keyboard=True)
+    await update.message.reply_text("Выберите активность", reply_markup=reply_markup)
+
+    return CHOOSE_ACTIVITY
+
+async def choose_activity_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data["ACTIVITY"] = update.message.text
+    activity = context.user_data["ACTIVITY"]
+    if activity == 'Создать новую':
+        await update.message.reply_text("Введите название активности:")
+        return ADD_ACTIVITY
+
+    activities = context.user_data["ACTIVITIES"]
+    activity_id = next(filter(lambda record: record[2] == activity, activities), None)[0]
+    context.user_data["ACTIVITY_ID"] = activity_id
+
+    await update.message.reply_text('Введите дату дедлайна в формате dd-mm-yyyy hh:mm')
+
+    return ADD_DEADLINE
 
 async def add_activity_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     context.user_data["ACTIVITY"] = update.message.text
+    activity = context.user_data["ACTIVITY"]
 
-    await update.message.reply_text('Введите дату дедлайна в формате yyyy-mm-dd hh:mm')
+    subject = context.user_data["SUBJECT"]
+    subject_id = context.user_data["SUBJECT_ID"]
+    sql = 'INSERT INTO ACTIVITIES(subject_id, name) VALUES(%s, %s) RETURNING id;'
+    result = run_sql(sql, (subject_id, activity))
+    if not result:
+        # Make it now await function
+        await update.message.reply_text(f"Активность с именем {activity} для предмета {subject} уже существует.")
+        return START
+
+    activity_id = result[0][0]
+    context.user_data["ACTIVITY_ID"] = activity_id
+
+    await update.message.reply_text('Введите дату дедлайна в формате dd-mm-yyyy hh:mm')
 
     return ADD_DEADLINE
 
 async def add_deadline_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     context.user_data["DEADLINE"] = update.message.text
 
-    activity_id = context.user_data["ACTIVITY"]
-    deadline = context.user_data["DEADLINE"]
+    activity_id = context.user_data["ACTIVITY_ID"]
 
+    deadline = context.user_data["DEADLINE"]
+    context.user_data.clear()
     try:
-        date = datetime.strptime(deadline, '%Y-%m-%d %H:%M')
+        date = datetime.strptime(deadline, '%d-%m-%Y %H:%M')
+        timestamp = int(date.timestamp())
     except ValueError:
         await update.message.reply_text('Неверный формат даты.')
         return ConversationHandler.END
-            
-    sql = "INSERT INTO deadlines1(activity_id, deadline) values (1, %s)"
-
-    cur = conn.cursor()
-    cur.execute(sql, (date,))
-    conn.commit()
+    
+    sql = "INSERT INTO deadlines(activity_id, deadline) values (%s, %s) ON CONFLICT (activity_id) DO UPDATE SET deadline = excluded.deadline;"
+    run_sql(sql, (activity_id, date))
 
     await update.message.reply_text('Дедлайн успешно обновлен!')
 
@@ -80,14 +135,23 @@ def add_deadline_builder() -> ConversationHandler:
     return ConversationHandler(
         entry_points=[CommandHandler("add_deadline", start_add_deadline_callback)],
         states={
+            CHOOSE_SUBJECT: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, choose_subject_callback)
+            ],
             ADD_SUBJECT: [
                 MessageHandler(filters.TEXT & ~filters.COMMAND, add_subject_callback)
+            ],
+            CHOOSE_ACTIVITY: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, choose_activity_callback)
             ],
             ADD_ACTIVITY: [
                 MessageHandler(filters.TEXT & ~filters.COMMAND, add_activity_callback)
             ],
             ADD_DEADLINE: [
                 MessageHandler(filters.TEXT & ~filters.COMMAND, add_deadline_callback)
+            ],
+            START: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, start_add_deadline_callback)
             ],
         },
         fallbacks=[CommandHandler("cancel", cancel_callback)]
