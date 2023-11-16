fa6ebbd890ff8a9db32f7b941ee6c827fb8141d8
CHAAAPS <vitars2001@gmail.com>
2023-11-16 17:44:37 +0300

    add subscription_list formatting

--- src/handlers/subscription_list_handler.py
+++ src/handlers/subscription_list_handler.py
@@ -14,17 +14,39 @@ from telegram.ext import (
 
 from src.handlers.handlers import cancel_callback
 
-from src.db.helpers import get_full_relation
+from src.db.helpers import run_sql
 
 
 async def subscription_list_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     msg = ""
-    db_result = get_full_relation("deadline_follows")
+    sql = (
+        f""" 
+               SELECT df.following_chat_id, s.name, a.name, d.deadline, d.activity_id, a.subject_id
+                   FROM deadline_follows AS df
+                       INNER JOIN activities AS a 
+                       ON df.subject_id = a.subject_id
+                       INNER JOIN deadlines AS d
+                       ON d.activity_id = a.id
+                       INNER JOIN subjects AS s 
+                       ON s.id = a.subject_id
+                   WHERE df.following_chat_id = %s; 
+               """
+    )
+    db_result = run_sql(sql, [update.message.chat.id])
+    print("db_result: " + str(db_result))
     for val in db_result:
-        msg = msg + str(val) + '\n' #форматирование добавить нужно
-    db_result = get_full_relation("google_sheets_follows")
+        msg = msg + f"Deadline for homework {val[1]} {val[2]} {str(val[3])} UTC" + '\n'
+
+    sql = (
+        f""" 
+                       SELECT table_id, cells_range 
+                       FROM google_sheets_follows
+                       WHERE following_chat_id = %s;
+                """
+    )
+    db_result = run_sql(sql, [update.message.chat.id])
     for val in db_result:
-        msg = msg + str(val) + '\n'
+        msg = msg + f"Subscription on table https://docs.google.com/spreadsheets/d/{val[0]} on cells {val[1]} " + '\n'
     await update.message.reply_text(msg)
 
     return ConversationHandler.END
